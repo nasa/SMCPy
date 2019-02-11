f49031d5f3cd95e8eae97da3d529d7b6a8e93ac3
peleser <patrick.e.leser@nasa.gov>
2019-02-11 10:09:25 -0500

    Updated SMC and MCMC versions of spring mass example.

--- examples/spring_mass/mcmc_verify/spring_mass_mcmc.py
+++ examples/spring_mass/mcmc_verify/spring_mass_mcmc.py
@@ -1,9 +1,11 @@
 import numpy as np
 from smcpy.smc.smc_sampler import SMCSampler
-from mcmcpy.mcmc.mcmc_sampler import MCMCSampler
+from smcpy.mcmc.mcmc_sampler import MCMCSampler
+
 import sys
 sys.path.append('../')
 from spring_mass_models import SpringMassModel
+
 
 # Initialize model
 state0 = [0., 0.]                        #initial conditions
@@ -11,18 +13,27 @@ measure_t_grid = np.arange(0., 5., 0.2)  #time
 model = SpringMassModel(state0, measure_t_grid)
 
 # Load data
-noise_stddev = 0.94
+noise_stddev = 0.5
 displacement_data = np.genfromtxt('../noisy_data.txt')
 
 # Define prior distributions
-param_priors = {'K': ['Uniform', 0.0, 20.0],
-                'g': ['Uniform', 0.0, 20.0]}
+initial_guess = {'K': 1.0, 'g': 1.0}
+param_priors = {'K': ['Uniform', 0.0, 10.0],
+                'g': ['Uniform', 0.0, 10.0]}
 
 # SMC sampling
-num_samples = 100000
-num_samples_burnin = 50000
+num_samples = 10000
+num_samples_burnin = 5000
 mcmc = MCMCSampler(displacement_data, model, param_priors)
-mcmc.generate_pymc_model(std_dev0=noise_stddev, fix_var=True)
+mcmc.generate_pymc_model(q0=initial_guess, std_dev0=noise_stddev, fix_var=True)
 mcmc.sample(num_samples, num_samples_burnin)
+
+# Calculate means
+Kmean = np.mean(mcmc.MCMC.trace('K')[:])
+gmean = np.mean(mcmc.MCMC.trace('g')[:])
+print '\nK mean = %s' % Kmean
+print 'g mean = %s\n' % gmean
+
+# Plot
 mcmc.plot_pairwise(keys=['K', 'g'])
 mcmc.plot_pdf(keys=['K', 'g'])
--- examples/spring_mass/spring_mass_example.py
+++ examples/spring_mass/spring_mass_example.py
@@ -22,5 +22,13 @@ num_mcmc_steps = 1
 smc = SMCSampler(displacement_data, model, param_priors)
 pchain = smc.sample(num_particles, num_time_steps, num_mcmc_steps, noise_stddev,
                     ess_threshold=num_particles*0.5)
+
+# Calculate means
+means = pchain.get_mean()
+print '\n'
+for key, value in means.iteritems():
+    print '%s mean = %s' % (key, value)
+
+# Plot
 if smc._rank == 0:
-    pchain.plot_pairwise_weights(save=True)
+    pchain.plot_pairwise_weights(save=True, show=False)
