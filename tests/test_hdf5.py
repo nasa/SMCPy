bc2d11cb36846d908bbda888c328c8358d9afdfd
Michael Wang <michael.wang@nasa.gov>
2019-02-20 16:37:02 -0500

    added all tests for hdf5_storage.py

--- tests/test_hdf5.py
+++ tests/test_hdf5.py
@@ -29,6 +29,12 @@ def h5file():
     return HDF5Storage('temp.hdf5', 'w')
 
 
+@pytest.fixture
+def filled_h5file(h5file, step_list):
+    h5file.write_step_list(step_list)
+    return h5file
+
+
 def test_write_particle(h5file, particle):
     h5file.write_particle(particle, step_index=1, particle_index=1)
     os.remove('temp.hdf5')
@@ -48,22 +54,45 @@ def test_write_step_list(h5file, step_list):
     os.remove('temp.hdf5')
 
 
-def test_read_particle(h5file, step_list):
-    h5file.write_step_list(step_list)
-    particle = h5file.read_particle(step_index=1, particle_index=0)
+def test_read_particle(filled_h5file):
+    particle = filled_h5file.read_particle(step_index=1, particle_index=0)
     assert particle.weight == 0.2
     os.remove('temp.hdf5')
 
 
-def test_read_step(h5file, step_list):
-    h5file.write_step_list(step_list)
-    step = h5file.read_step(1)
+def test_read_step(filled_h5file):
+    step = filled_h5file.read_step(step_index=1)
     assert step[0].weight == 0.2
     os.remove('temp.hdf5')
 
 
-def test_read_step_list(h5file, step_list):
-    h5file.write_step_list(step_list)
-    step_list = h5file.read_step_list()
-    assert step_list[0].particles[0].weight == 0.2
+def test_read_step_list(filled_h5file):
+    step_list = filled_h5file.read_step_list()
+    weight = step_list[0].particles[0].weight
+    assert weight == 0.2
+    os.remove('temp.hdf5')
+
+
+def test_close(h5file, step_list):
+    h5file.close()
+    with pytest.raises(ValueError):
+        h5file.write_step_list(step_list)
+    os.remove('temp.hdf5')
+
+
+def test_get_num_steps(filled_h5file):
+    num_steps = filled_h5file.get_num_steps()
+    assert num_steps == 3
+    os.remove('temp.hdf5')
+
+
+def test_get_num_particles_in_step(filled_h5file):
+    num_particles = filled_h5file.get_num_particles_in_step(step_index=1)
+    assert num_particles == 5
+    os.remove('temp.hdf5')
+
+
+def test_create_step_group(h5file):
+    h5file._create_step_group(1)
+    assert h5file._h5.keys() == ['steps']
     os.remove('temp.hdf5')
