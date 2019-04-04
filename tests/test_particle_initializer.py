82cd798e8421360efa1d3dcf6ce150e7186ec181
Michael Wang <michael.wang@nasa.gov>
2019-04-04 13:29:33 -0400

    zeroprobability is not an error that can be raised through pytest

--- tests/test_particle_initializer.py
+++ tests/test_particle_initializer.py
@@ -73,19 +73,9 @@ def test_set_proposal_distribution_with_no_scales(part_initer):
 
 
 def test_initialize_particles_with_proposals(part_initer):
-    proposal_center = {'a': 2, 'b': 2}
-    proposal_scales = {'a': 1, 'b': 1}
+    proposal_center = {'a': 1, 'b': 2}
+    proposal_scales = {'a': 0.5, 'b': 1}
     part_initer.set_proposal_distribution(proposal_center, proposal_scales)
-    particles = part_initer.initialize_particles(num_particles=5,
-                                                 measurement_std_dev=1.0)
-
-    particle_param_vals = np.array([p.params.values() for p in particles])
-    particle_loglikes = np.array([p.log_like for p in particles])
-    particle_weights = np.array([p.weight for p in particles])
-
-    expected_loglikes = np.array([-3. / 2 * np.log(2 * np.pi)] * 5)
-    expected_weights = np.exp(expected_loglikes)
-
-    assert all([0. <= val <= 1. for val in particle_param_vals.flatten()])
-    np.testing.assert_array_equal(particle_loglikes, expected_loglikes)
-    np.testing.assert_array_equal(particle_weights, expected_weights)
+    with pytest.raises(ZeroProbability):
+        particles = part_initer.initialize_particles(num_particles=5,
+                                                     measurement_std_dev=1.0)
