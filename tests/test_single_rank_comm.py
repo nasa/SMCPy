74d1acb31cc22f8deed4b2c4ff8586255cfb28ba
Michael Wang <michael.wang@nasa.gov>
2019-04-05 16:37:52 -0400

    gather test in single_rank_comm test

--- tests/test_single_rank_comm.py
+++ tests/test_single_rank_comm.py
@@ -18,7 +18,7 @@ def test_get_size(single_rank_comm):
     assert single_rank_comm.Get_size() == 1
 
 
-def test_when_length_scatter_list_one(single_rank_comm):
+def test_scatter_when_length_scatter_list_one(single_rank_comm):
     scatter_list = [np.array([0, 1, 2, 3])]
     array_equal(single_rank_comm.scatter(scatter_list), np.array([0, 1, 2, 3]))
 
@@ -27,3 +27,8 @@ def test_raise_error_when_length_scatter_list_not_one(single_rank_comm):
     scatter_list = np.array([0, 1, 2, 3])
     with pytest.raises(ValueError):
         single_rank_comm.scatter(scatter_list)
+
+
+def test_gather(single_rank_comm):
+    scatter_list = np.array([0, 1, 2, 3])
+    array_equal(single_rank_comm.gather(scatter_list), scatter_list)
