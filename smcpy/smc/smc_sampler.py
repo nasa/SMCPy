eae9c1fb409ab406fa96740a30cb25ce64e92780
Michael Wang <michael.wang@nasa.gov>
2019-04-23 15:34:02 -0400

    added back in load step list

--- smcpy/smc/smc_sampler.py
+++ smcpy/smc/smc_sampler.py
@@ -193,3 +193,21 @@ class SMCSampler(Properties):
         if self._rank == 0 and self._autosaver is not None:
             self.autosaver.close()
         return None
+
+    def load_step_list(self, h5_file):
+        '''
+        Loads and returns a step list stored using the HDF5Storage
+        class.
+
+        :param hdf5_to_load: file path of a step_list saved using the
+            self.save_step_list() methods.
+        :type hdf5_to_load: string
+        '''
+        if self._rank == 0:
+            hdf5 = HDF5Storage(h5_file, mode='r')
+            step_list = hdf5.read_step_list()
+            hdf5.close()
+            print 'Step list loaded from %s.' % h5_file
+        else:
+            step_list = None
+        return step_list
