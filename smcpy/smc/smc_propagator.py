c82d9c666cf84259d432ddb888568b3725c784a4
peleser <patrick.e.leser@nasa.gov>
2019-09-13 14:36:23 -0400

    added ability to specify names for outputs when using propagator

--- smcpy/smc/smc_propagator.py
+++ smcpy/smc/smc_propagator.py
@@ -2,12 +2,13 @@ from .smc_step import SMCStep
 
 class SMCPropagator():
 
-    def __init__(self, model):
+    def __init__(self, model, output_names=None):
         '''
         :param model: predictive model to propagate particles through
         :type model: BaseModel object
         '''
         self._model = model
+        self._output_names = output_names
 
     def propagate(self, smc_step):
         '''
@@ -22,8 +23,22 @@ class SMCPropagator():
         particle_list = smc_step.get_particles()
         for particle in particle_list:
             outputs = self._model.evaluate(particle.params)
-            particle.params = {'output_{}'.format(i): out \
-                               for i, out in enumerate(outputs)}
+            particle.params = self._create_output_dictionary(outputs)
+
         new_step = SMCStep()
         new_step.set_particles(particle_list)
         return new_step
+
+    def _create_output_dictionary(self, outputs):
+        output_length = len(outputs)
+        if self._output_names is None:
+            output_names = ['output_{}'.format(i) for i in range(output_length)]
+        else:
+            self._check_len_of_output_names(output_length)
+            output_names = self._output_names
+        return {output_names[i]: out for i, out in enumerate(outputs)}
+
+    def _check_len_of_output_names(self, output_length):
+        if len(self._output_names) < output_length:
+            raise ValueError('not enough names in output_names for output ' + \
+                             'array of length {}'.format(output_length))
