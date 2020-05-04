d37095ead4c44321eb470011c944b257c413276f
peleser <patrick.e.leser@nasa.gov>
2020-05-04 18:02:03 -0400

    refactored initializer and added unit tests

---
+++ smcpy/smc/initializer.py
@@ -0,0 +1,140 @@
+'''
+Notices:
+Copyright 2018 United States Government as represented by the Administrator of
+the National Aeronautics and Space Administration. No copyright is claimed in
+the United States under Title 17, U.S. Code. All Other Rights Reserved.
+
+Disclaimers
+No Warranty: THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF
+ANY KIND, EITHER EXPRessED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED
+TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY
+IMPLIED WARRANTIES OF MERCHANTABILITY, FITNess FOR A PARTICULAR PURPOSE, OR
+FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL BE ERROR
+FREE, OR ANY WARRANTY THAT DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE
+SUBJECT SOFTWARE. THIS AGREEMENT DOES NOT, IN ANY MANNER, CONSTITUTE AN
+ENDORSEMENT BY GOVERNMENT AGENCY OR ANY PRIOR RECIPIENT OF ANY RESULTS,
+RESULTING DESIGNS, HARDWARE, SOFTWARE PRODUCTS OR ANY OTHER APPLICATIONS
+RESULTING FROM USE OF THE SUBJECT SOFTWARE.  FURTHER, GOVERNMENT AGENCY
+DISCLAIMS ALL WARRANTIES AND LIABILITIES REGARDING THIRD-PARTY SOFTWARE, IF
+PRESENT IN THE ORIGINAL SOFTWARE, AND DISTRIBUTES IT "AS IS."
+
+Waiver and Indemnity:  RECIPIENT AGREES TO WAIVE ANY AND ALL CLAIMS AGAINST THE
+UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS ANY
+PRIOR RECIPIENT.  IF RECIPIENT'S USE OF THE SUBJECT SOFTWARE RESULTS IN ANY
+LIABILITIES, DEMANDS, DAMAGES, EXPENSES OR LOSSES ARISING FROM SUCH USE,
+INCLUDING ANY DAMAGES FROM PRODUCTS BASED ON, OR RESULTING FROM, RECIPIENT'S
+USE OF THE SUBJECT SOFTWARE, RECIPIENT SHALL INDEMNIFY AND HOLD HARMLess THE
+UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS ANY
+PRIOR RECIPIENT, TO THE EXTENT PERMITTED BY LAW.  RECIPIENT'S SOLE REMEDY FOR
+ANY SUCH MATTER SHALL BE THE IMMEDIATE, UNILATERAL TERMINATION OF THIS
+AGREEMENT.
+'''
+
+from copy import copy
+from ..mcmc.translator_base import Translator
+from ..particles.particle import Particle
+from ..smc.smc_step import SMCStep
+from ..utils.single_rank_comm import SingleRankComm
+import numpy as np
+import warnings
+
+
+class Initializer():
+    '''
+    Generates particle lists based on either samples from a prior distribution
+    or given input samples from a sampling distribution.
+    '''
+    def __init__(self, mcmc_kernel, phi_init, mpi_comm=SingleRankComm()):
+        self.phi_init = phi_init
+        self._size = mpi_comm.Get_size()
+        self._rank = mpi_comm.Get_rank()
+        self.mcmc_kernel = mcmc_kernel
+
+    def get_num_particles_in_partition(self, total_num_particles, rank):
+        '''
+        Get number of particles in partition based on MPI rank and total
+        number of particles requested.
+
+        :param total_num_particles: number of particles accross all ranks
+        :type total_num_particles: int
+        :param rank: MPI rank
+        :type rank: int
+        '''
+        num_particles_for_partition = int(total_num_particles / self._size)
+        remainder = total_num_particles % self._size
+        overtime_ranks = range(remainder)
+        if rank in overtime_ranks:
+            num_particles_for_partition += 1
+        return num_particles_for_partition
+
+    def initialize_particles_from_prior(self, num_particles):
+        '''
+        Use model stored in MCMC kernel to sample initial set of particles.
+
+        :param num_particles: number of particles to sample (total across all
+            ranks)
+        :type num_particles: int
+        '''
+        n_particles_in_part = self.get_num_particles_in_partition(num_particles,
+                                                                  self._rank)
+        prior_samples = self.mcmc_kernel.sample_from_prior(n_particles_in_part)
+        param_names = list(prior_samples.keys())
+        param_values = np.array([prior_samples[pn] for pn in param_names]).T
+
+        particles = []
+        for vals in param_values:
+            params = dict(zip(param_names, vals))
+            log_like = self.mcmc_kernel.get_log_likelihood(params)
+            non_norm_log_weight = log_like * self.phi_init
+            particles.append(Particle(params, non_norm_log_weight, log_like))
+
+        return particles
+
+    def initialize_particles_from_samples(self, samples, proposal_pdensity):
+        '''
+        Initialize a set of particles using pre-sampled parameter values and
+        the corresponding prior pdf at those values.
+
+        :param samples: samples of parameters used to initialize particles;
+            must be a dictionary with keys = parameter names and values =
+            parameter values. Can also be a pandas DataFrame object for this
+            reason.
+        :type samples: dict, pandas.DataFrame
+        :param proposal_pdensity: corresponding probability density function
+            values; must be aligned with samples
+        :type proposal_pdensity: list or nd.array
+        '''
+        num_particles = len(proposal_pdensity)
+        n_particles_in_parts = \
+            [self.get_num_particles_in_partition(num_particles, rank) for \
+             rank in range(self._size)]
+
+        param_names = list(samples.keys())
+        param_values = np.array([samples[pn] for pn in param_names]).T
+        log_proposal_pdensity = np.log(proposal_pdensity)
+
+        start_index = int(np.sum(n_particles_in_parts[:self._rank]))
+        end_index = start_index + n_particles_in_parts[self._rank]
+        param_values = param_values[start_index:end_index]
+        log_proposal_pdensity = log_proposal_pdensity[start_index:end_index]
+
+        particles = []
+        for i, vals in enumerate(param_values):
+            params = dict(zip(param_names, vals))
+            log_like = self.mcmc_kernel.get_log_likelihood(params)
+            log_prior = self.mcmc_kernel.get_log_prior(params)
+            non_norm_log_weight = log_like * self.phi_init + log_prior - \
+                                  log_proposal_pdensity[i]
+            particles.append(Particle(params, non_norm_log_weight, log_like))
+
+        return particles
+
+    @property
+    def mcmc_kernel(self):
+        return self._mcmc_kernel
+
+    @mcmc_kernel.setter
+    def mcmc_kernel(self, mcmc_kernel):
+        if not isinstance(mcmc_kernel, Translator):
+            raise TypeError
+        self._mcmc_kernel = mcmc_kernel
---
+++ tests/unit/test_initializer.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas
+import pytest
+
+from smcpy.smc.initializer import Initializer
+from smcpy.mcmc.translator_base import Translator
+
+
+@pytest.fixture
+def stub_comm(mocker):
+    comm = mocker.Mock()
+    return comm
+
+
+@pytest.fixture
+def stub_mcmc_kernel(mocker):
+    stub_mcmc_kernel = mocker.Mock(Translator)
+    mocker.patch.object(stub_mcmc_kernel, 'sample_from_prior', create=True,
+                        return_value={'a': [1, 1, 1, 2, 2]})
+    mocker.patch.object(stub_mcmc_kernel, 'get_log_likelihood', create=True,
+                        side_effect=[0.1, 0.1, 0.1, 0.2, 0.2])
+    mocker.patch.object(stub_mcmc_kernel, 'get_log_prior', create=True,
+                        side_effect=[0.2, 0.2, 0.2, 0.3, 0.3])
+    return stub_mcmc_kernel
+
+
+@pytest.fixture
+def initializer(stub_mcmc_kernel, stub_comm, mocker):
+    initializer = Initializer(stub_mcmc_kernel, phi_init=2, mpi_comm=stub_comm)
+    return initializer
+
+
+@pytest.mark.parametrize('rank,expected', [(0, 4), (1, 3), (2, 3)])
+def test_get_num_particles_in_partition(stub_comm, stub_mcmc_kernel, rank,
+                                         expected, mocker):
+    mocker.patch.object(stub_comm, 'Get_size', return_value=3)
+    mocker.patch.object(stub_comm, 'Get_rank', return_value=rank)
+    initializer = Initializer(stub_mcmc_kernel, None, mpi_comm=stub_comm)
+    assert initializer.get_num_particles_in_partition(10, rank) == expected
+
+
+def test_mcmc_kernel_not_translator_instance():
+    with pytest.raises(TypeError):
+        initializer = Initializer(None, None)
+
+
+def test_initialize_particles_from_prior(initializer, mocker):
+    mocker.patch.object(initializer, 'get_num_particles_in_partition',
+                        new=lambda x, y: x)
+    particles = initializer.initialize_particles_from_prior(5)
+
+    expected_a_vals = [1, 1, 1, 2, 2]
+    expected_log_like = [0.1, 0.1, 0.1, 0.2, 0.2]
+    expected_log_weight = [0.2] * 3 + [0.4] * 2
+
+    np.testing.assert_array_almost_equal([p.params['a'] for p in particles],
+                                         expected_a_vals)
+    np.testing.assert_array_almost_equal([p.log_like for p in particles],
+                                         expected_log_like)
+    np.testing.assert_array_almost_equal([p.log_weight for p in particles],
+                                         expected_log_weight)
+
+
+@pytest.mark.parametrize('rank,expected_params,dataframe',
+                         [(0, {'a': [3, 3, 3, 4, 4], 'b': [1, 1, 1, 2, 2]}, 0),
+                          (1, {'a': [1, 1, 1, 2, 2], 'b': [2, 2, 2, 5, 5]}, 0),
+                          (1, {'a': [1, 1, 1, 2, 2], 'b': [2, 2, 2, 5, 5]}, 1),
+                          (2, {'a': [3, 4, 5, 5], 'b': [1, 2, 3, 4]}, 0)])
+def test_initialize_particles_from_samples(rank, expected_params, initializer,
+                                           dataframe):
+    samples = {'a': np.array([3, 3, 3, 4, 4, 1, 1, 1, 2, 2, 3, 4, 5, 5]),
+               'b': np.array([1, 1, 1, 2, 2, 2, 2, 2, 5, 5, 1, 2, 3, 4])}
+
+    if dataframe:
+        samples = pandas.DataFrame(samples)
+
+    proposal_pdensity = np.array(samples['a']) * 0.1
+
+    initializer._size = 3
+    initializer._rank = rank
+
+    particles = initializer.initialize_particles_from_samples(samples,
+                                                              proposal_pdensity)
+
+    expected_length = len(expected_params['a'])
+    expected_log_like = [0.1, 0.1, 0.1, 0.2, 0.2][:expected_length]
+    expected_log_prior = np.array([0.4] * 3 + [0.7] * 2)[:expected_length]
+    expected_log_prop = np.log(np.array(expected_params['a']) * 0.1)
+    expected_log_weight = expected_log_prior - expected_log_prop
+
+    np.testing.assert_array_almost_equal([p.params['a'] for p in particles],
+                                          expected_params['a'])
+    np.testing.assert_array_almost_equal([p.params['b'] for p in particles],
+                                          expected_params['b'])
+    np.testing.assert_array_almost_equal([p.log_like for p in particles],
+                                         expected_log_like)
+    np.testing.assert_array_almost_equal([p.log_weight for p in particles],
+                                         expected_log_weight)
