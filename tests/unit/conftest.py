6a5d9e4a9585a4918e90250c7a587bbcfc9b04a9
peleser <patrick.e.leser@nasa.gov>
2020-05-11 14:36:23 -0400

    updated conftest for use in mutator unit test

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -18,4 +18,9 @@ def stub_mcmc_kernel(mocker):
                         side_effect=[0.1, 0.1, 0.1, 0.2, 0.2])
     mocker.patch.object(stub_mcmc_kernel, 'get_log_prior', create=True,
                         side_effect=[0.2, 0.2, 0.2, 0.3, 0.3])
+    mocker.patch.object(stub_mcmc_kernel, 'sample', create=True)
+    mocker.patch.object(stub_mcmc_kernel, 'get_final_trace_values', create=True,
+                        side_effect=[{'a': 1, 'b': 3},
+                                     {'a': 2, 'b': 4},
+                                     {'a': 5, 'b': 6}])
     return stub_mcmc_kernel
