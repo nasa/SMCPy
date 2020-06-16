3ce74d351229f7ba3cf0b7e2b607462b06d9ba1e
Leser <pleser@ndc.nasa.gov>
2020-06-16 07:59:13 -0400

    prior pdfs are now member variables

---
+++ smcpy/mcmc/vector_mcmc.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+class VectorMCMC:
+    
+    def __init__(self, model, data, prior_pdfs):
+        self._model = model
+        self._data = data
+        self._prior_pdfs = prior_pdfs
+
+    def evaluate_log_likelihood(self, inputs, std_dev):
+        if std_dev is None:
+            std_dev = inputs[:, -1]
+        var = std_dev ** 2
+        output = self._model(inputs)
+        data = np.tile(self._data, [inputs.shape[0], 1])
+        ssqe = np.sum((output - data) ** 2, axis=1)
+
+        term1 = np.log(2 * np.pi / var) ** (len(data) / 2.) 
+        term2 = -1 / 2. * ssqe / var
+        return (term1 + term2).reshape(-1, 1)
+
+    def evaluate_log_priors(self, inputs):
+        log_priors = [np.log(p(inputs.T[i]).reshape(-1, 1)) \
+                      for i, p in enumerate(self._prior_pdfs)]
+        return np.hstack(log_priors)
+
+    @staticmethod
+    def evaluate_posterior(log_likelihood, log_priors):
+        return np.sum(np.hstack((log_likelihood, log_priors)), axis=1)
+
+    @staticmethod
+    def proposal(inputs, cov):
+        mean = np.zeros(cov.shape[0])
+        delta = np.random.multivariate_normal(mean, cov, inputs.shape[0])
+        return inputs + delta
+
+    def acceptance_ratio(self, new_log_like, old_log_like, new_log_priors,
+                         old_log_priors):
+        old_log_post = self.evaluate_posterior(old_log_like, old_log_priors)
+        new_log_post = self.evaluate_posterior(new_log_like, new_log_priors)
+        return np.exp(new_log_post - old_log_post).reshape(-1, 1)
+
+    @staticmethod
+    def selection(new_values, old_values, acceptance_ratios, u):
+        reject = acceptance_ratios < u
+        return np.where(reject, old_values, new_values)
+
+    def smc_metropolis(self, num_samples, inputs, cov, std_dev=None):
+        log_like = self.evaluate_log_likelihood(inputs, std_dev)
+        log_priors = self.evaluate_log_priors(inputs)
+    
+        for i in range(num_samples):
+    
+            new_inputs = self.proposal(inputs, cov)
+            new_log_like = self.evaluate_log_likelihood(new_inputs, std_dev)
+            new_log_priors = self.evaluate_log_priors(new_inputs)
+    
+            accpt_ratio = self.acceptance_ratio(new_log_like, log_like,
+                                                new_log_priors, log_priors)
+    
+            u = np.random.uniform(0, 1, accpt_ratio.shape)
+    
+            inputs = self.selection(new_inputs, inputs, accpt_ratio, u)
+            log_like = self.selection(new_log_like, log_like, accpt_ratio, u)
+            log_priors = self.selection(new_log_priors, log_priors,
+                                        accpt_ratio, u)
+    
+        return inputs, log_like
---
+++ tests/unit/test_vector_mcmc.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pytest
+import sys
+
+from smcpy.mcmc.vector_mcmc import *
+
+@pytest.fixture
+def stub_model():
+    x = np.array([1, 2, 3])
+    def evaluation(q):
+        output = (q[:, 0, None] * 2 + 3.25 * q[:, 1, None] - \
+                  q[:, 2, None] ** 2) * x
+        return output
+    return evaluation
+
+
+@pytest.fixture
+def data():
+    return np.array([5, 4, 9])
+
+
+@pytest.fixture
+def prior_pdfs():
+    return [lambda x: x, lambda x: x + 1, lambda x: x *2]
+
+
+@pytest.fixture
+def vector_mcmc(stub_model, data, prior_pdfs):
+    return VectorMCMC(stub_model, data, prior_pdfs)
+
+
+@pytest.mark.parametrize('inputs, std_dev', (
+                        ((np.array([[0, 1, 0.5]]), np.sqrt(2 * np.pi)),
+                         (np.array([[0, 1, 0.5]] * 4), np.sqrt(2 * np.pi)),
+                         (np.array([[0, 1, 0.5, np.sqrt(2 * np.pi)]] * 4), None)
+                        )))
+def test_vectorized_likelihood(vector_mcmc, inputs, std_dev):
+    expected_like = np.array(inputs.shape[0] * [[np.exp(-2 / np.pi)]])
+    expected_log_like = np.log(expected_like)
+
+    log_like = vector_mcmc.evaluate_log_likelihood(inputs, std_dev)
+
+    np.testing.assert_array_almost_equal(log_like, expected_log_like)
+
+
+@pytest.mark.parametrize('inputs', (np.array([[0.1, 1, 0.5]]),
+                                    np.array([[0.1, 1, 0.5]] * 4)))
+def test_vectorized_prior(vector_mcmc, inputs):
+    log_prior = vector_mcmc.evaluate_log_priors(inputs)
+
+    expected_log_prior = np.log(np.array([[0.1, 2, 1]] * inputs.shape[0]))
+    np.testing.assert_array_almost_equal(log_prior, expected_log_prior)
+
+
+@pytest.mark.parametrize('inputs', (np.array([[0, 1, 0.5]]),
+                                    np.array([[0, 1, 0.5]] * 4)))
+def test_vectorized_proposal(vector_mcmc, inputs, mocker):
+    mvn_mock = mocker.patch('numpy.random.multivariate_normal',
+                            return_value=np.ones(inputs.shape))
+    cov = np.eye(inputs.shape[1])
+    expected = inputs + 1
+
+    inputs_new = vector_mcmc.proposal(inputs, cov=cov)
+
+    calls = mvn_mock.call_args[0]
+    np.testing.assert_array_equal(inputs_new, expected)
+    np.testing.assert_array_equal(calls[0], np.zeros(cov.shape[0]))
+    np.testing.assert_array_equal(calls[1], cov)
+    np.testing.assert_array_equal(calls[2], inputs.shape[0])
+
+
+@pytest.mark.parametrize('new_inputs, old_inputs', (
+                        (np.array([[1, 1, 1]]), np.array([[2, 2, 2]])),
+                        (np.array([[1, 1, 1]] * 4), np.array([[2, 2, 2]] * 4))))
+def test_vectorized_accept_ratio(vector_mcmc, new_inputs, old_inputs, mocker):
+    mocked_new_log_likelihood = np.ones([new_inputs.shape[0], 1])
+    mocked_old_log_likelihood = np.ones([new_inputs.shape[0], 1])
+    mocked_new_log_priors = new_inputs
+    mocked_old_log_priors = old_inputs
+
+    accpt_ratio = vector_mcmc.acceptance_ratio(mocked_new_log_likelihood,
+                                               mocked_old_log_likelihood,
+                                               mocked_new_log_priors,
+                                               mocked_old_log_priors)
+
+    expected = np.exp(np.array([[4 - 7.]] * new_inputs.shape[0]))
+    np.testing.assert_array_equal(accpt_ratio, expected)
+
+
+@pytest.mark.parametrize('new_inputs, old_inputs', (
+                        (np.array([[1, 1, 1]]), np.array([[2, 2, 2]])),
+                        (np.array([[1, 1, 1]] * 4), np.array([[2, 2, 2]] * 4))))
+def test_vectorized_selection(vector_mcmc, new_inputs, old_inputs, mocker):
+    mocked_uniform_samples = np.ones([new_inputs.shape[0], 1]) * 0.5
+    acceptance_ratios = np.c_[[0.25, 1.2, 0.25, 0.75]][:new_inputs.shape[0]]
+
+    accepted_inputs = vector_mcmc.selection(new_inputs, old_inputs,
+                                            acceptance_ratios,
+                                            mocked_uniform_samples)
+
+    expected = np.array([[2, 2, 2], [1, 1, 1], [2, 2, 2], [1, 1, 1]])
+    np.testing.assert_array_equal(accepted_inputs,
+                                  expected[:new_inputs.shape[0]])
+
+
+@pytest.mark.parametrize('num_samples', (1, 2))
+def test_vectorized_smc_metropolis(vector_mcmc, num_samples, mocker):
+    inputs = np.ones([10, 3])
+    cov = np.eye(3)
+    std_dev = 1
+    vector_mcmc._prior_pdfs = [mocker.Mock(), mocker.Mock(), mocker.Mock()]
+
+    mocker.patch('numpy.random.uniform')
+    mocker.patch.object(vector_mcmc, 'acceptance_ratio')
+    mocker.patch.object(vector_mcmc, 'proposal')
+    log_like = mocker.patch.object(vector_mcmc, 'evaluate_log_likelihood',
+                                   return_value=inputs * 2)
+    mocker.patch.object(vector_mcmc, 'selection',
+                        side_effect= [inputs + 1, inputs * 2, None,
+                                      inputs + 2, inputs * 2, None])
+
+    new_inputs, new_log_likes = vector_mcmc.smc_metropolis(num_samples, inputs,
+                                                           cov, std_dev)
+
+    expected_new_inputs = inputs + num_samples
+    expected_new_log_likes = inputs * 2
+
+    np.testing.assert_array_equal(new_inputs, expected_new_inputs)
+    np.testing.assert_array_equal(new_log_likes, expected_new_log_likes)
+
+    assert log_like.call_count == num_samples + 1
+    assert vector_mcmc._prior_pdfs[0].call_count == num_samples + 1
