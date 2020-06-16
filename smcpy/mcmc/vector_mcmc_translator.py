55a7591d0bec7c6a7e743bf32f97024fa03b62c3
Leser <pleser@ndc.nasa.gov>
2020-06-16 17:51:16 -0400

    added tests for vector mcmc translator, almost done; missing a called with check

---
+++ smcpy/mcmc/vector_mcmc_translator.py
@@ -0,0 +1,38 @@
+from copy import copy
+import numpy as np
+
+from .translator_base import Translator
+
+class VectorMCMCTranslator(Translator):
+
+    def __init__(self, vector_mcmc_object, param_order):
+        self._mcmc = vector_mcmc_object
+        self._param_order = param_order
+
+    def mutate_particles(self, particles, num_samples, proposal_cov, phi):
+        particles = copy(particles)
+
+        input_array = np.zeros([len(particles), len(self._param_order)])
+        for i, p in enumerate(particles):
+            input_array[i, :] = [p.params[key] for key in self._param_order]
+
+        params, log_likes = self._mcmc.smc_metropolis(num_samples, input_array,
+                                                      proposal_cov, phi)
+
+        for i, p in enumerate(particles):
+            p.params = {key: params[i][j] \
+                        for j, key in enumerate(self._param_order)}
+            p.log_like = log_likes[i, 0]
+
+        return particles
+
+    def get_log_likelihood(self, param_dict):
+        input_array = self._translate_param_dict_to_input_array(param_dict)
+        return self._mcmc.evaluate_log_likelihood(input_array)
+
+    def get_log_prior(self, param_dict):
+        input_array = self._translate_param_dict_to_input_array(param_dict)
+        return self._mcmc.evaluate_log_priors(input_array)
+
+    def _translate_param_dict_to_input_array(self, param_dict):
+        return np.array([[param_dict[k] for k in self._param_order]])
---
+++ tests/unit/test_vector_mcmc_translator.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pytest
+
+from smcpy.mcmc.vector_mcmc import VectorMCMC
+from smcpy.mcmc.vector_mcmc_translator import VectorMCMCTranslator
+
+@pytest.fixture
+def stub_model(mocker):
+    return mocker.Mock()
+
+
+@pytest.fixture
+def data(mocker):
+    return mocker.Mock()
+
+
+@pytest.fixture
+def prior_pdfs(mocker):
+    return mocker.Mock()
+
+
+@pytest.fixture
+def vector_mcmc(stub_model, data, prior_pdfs):
+    return VectorMCMC(stub_model, data, prior_pdfs)
+
+
+def test_translator_instance(vector_mcmc, stub_model, data, prior_pdfs):
+    vmcmc = VectorMCMCTranslator(vector_mcmc, tuple('a'))
+    assert vmcmc._mcmc._model == stub_model
+    assert vmcmc._mcmc._data == data
+    assert vmcmc._mcmc._prior_pdfs == prior_pdfs
+    assert vmcmc._param_order == tuple('a')
+
+
+@pytest.mark.parametrize('param_dict, expected',
+                         ([{'a': 1, 'b': 2}, np.array([[1]])],
+                          [{'a': [1, 1], 'b': [1, 2]}, np.array([[1], [1]])]))
+def test_translator_log_likelihoods(vector_mcmc, param_dict, expected, mocker):
+    vmcmc = VectorMCMCTranslator(vector_mcmc, param_order=['a', 'b'])
+    mocker.patch.object(vector_mcmc, 'evaluate_log_likelihood',
+                        new=lambda x: x[:, 0].reshape(-1, 1))
+    
+    log_likes = vmcmc.get_log_likelihood(param_dict)
+    np.testing.assert_array_equal(log_likes, expected)
+
+
+@pytest.mark.parametrize('param_dict, expected',
+                         ([{'a': 1, 'b': 2}, np.array([[1]])],
+                          [{'a': [1, 1], 'b': [1, 2]}, np.array([[1], [1]])]))
+def test_translator_log_priors(vector_mcmc, param_dict, expected, mocker):
+    vmcmc = VectorMCMCTranslator(vector_mcmc, param_order=['b', 'a'])
+    mocker.patch.object(vector_mcmc, 'evaluate_log_priors',
+                        new=lambda x: x[:, 1].reshape(-1, 1))
+    
+    log_priors = vmcmc.get_log_prior(param_dict)
+    np.testing.assert_array_equal(log_priors, expected)
+
+
+@pytest.mark.parametrize('num_vectorized', (1, 5))
+def test_mutate_particles(vector_mcmc, num_vectorized, mocker):
+    num_samples = 2
+    proposal_cov = np.eye(2)
+    phi = 1
+
+    particles = [mocker.Mock()] * num_vectorized
+    for p in particles:
+        p.params = {'a': 1, 'b': 2}
+        p.log_like = 1
+
+    mocked_return = (np.array([[2, 3]] * num_vectorized),
+                     np.array([[2]] * num_vectorized))
+    smc_metropolis = mocker.patch.object(vector_mcmc, 'smc_metropolis',
+                                         return_value=mocked_return)
+
+    vmcmc = VectorMCMCTranslator(vector_mcmc, param_order=['a', 'b'])
+    mutated = vmcmc.mutate_particles(particles, num_samples, proposal_cov, phi)
+
+    expected_input_array = np.array([[1, 2]] * num_vectorized)
+    expected_particles = [mocker.Mock()] * num_vectorized
+    for p in expected_particles:
+        p.params = {'a': 2, 'b': 3}
+        p.log_like = 2
+
+    np.testing.assert_array_equal([p.params for p in mutated],
+                                  [p.params for p in expected_particles])
+    np.testing.assert_array_equal([p.log_like for p in mutated],
+                                  [p.log_like for p in expected_particles])
+
+    # NEED AN ASSERT CALLED WITH FOR SMC_METROPOLIS
