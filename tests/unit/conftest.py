7f060c164f4383711cfccf02810b2e6716f50845
Leser <pleser@ndc.nasa.gov>
2020-06-18 08:11:17 -0400

    removed extraneous patches of stub_mcmc_kernel for translator tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -13,15 +13,8 @@ def stub_comm(mocker):
 @pytest.fixture
 def stub_mcmc_kernel(mocker):
     stub_mcmc_kernel = mocker.Mock(Translator)
-    mocker.patch.object(stub_mcmc_kernel, 'sample_from_prior', create=True,
-                        return_value={'a': [1, 1, 1, 2, 2]})
     mocker.patch.object(stub_mcmc_kernel, 'get_log_likelihood', create=True,
                         side_effect=[0.1, 0.1, 0.1, 0.2, 0.2])
     mocker.patch.object(stub_mcmc_kernel, 'get_log_prior', create=True,
                         side_effect=[0.2, 0.2, 0.2, 0.3, 0.3])
-    mocker.patch.object(stub_mcmc_kernel, 'sample', create=True)
-    mocker.patch.object(stub_mcmc_kernel, 'get_final_trace_values', create=True,
-                        side_effect=[{'a': 1, 'b': 3},
-                                     {'a': 2, 'b': 4},
-                                     {'a': 5, 'b': 6}])
     return stub_mcmc_kernel
