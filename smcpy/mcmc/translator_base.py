6cf03d5bb592724cf68464fa94b86bf036fe64e4
Leser <pleser@ndc.nasa.gov>
2020-06-18 08:09:28 -0400

    defined translator base class with minimum required methods

--- smcpy/mcmc/translator_base.py
+++ smcpy/mcmc/translator_base.py
@@ -1,4 +1,21 @@
-class Translator:
+import abc
 
-    def __init__(self,):
+# compatible with Python 2 *and* 3:
+ABC = abc.ABCMeta('ABC', (object,), {'__slots__': ()})
+
+class Translator(ABC):
+
+    def __init__(self):
         pass
+
+    @abc.abstractmethod
+    def mutate_particles(self, particles, num_samples, proposal_cov, phi):
+        return new_param_values, new_log_likelihoods
+
+    @abc.abstractmethod
+    def get_log_likelihood(self, param_dict):
+        return log_likelihoods
+
+    @abc.abstractmethod
+    def get_log_prior(self, param_dict):
+        return log_priors
