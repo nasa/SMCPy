fd21a112261e9019a4887956ca1f42c907754045
Leser <pleser@ndc.nasa.gov>
2020-12-02 14:30:29 -0500

    multisource likelihood tested for fixed and variable std

--- smcpy/log_likelihoods.py
+++ smcpy/log_likelihoods.py
@@ -2,6 +2,11 @@ import numpy as np
 
 
 def normal(inputs, model, data, args):
+    '''
+    Likelihood function for data with additive, iid errors sampled from a
+    normal distribution with mean = 0 and std_dev = args. If args is None,
+    assumes that the last column of inputs contains the std_dev value.
+    '''
     std_dev = args
     if std_dev is None:
         std_dev = inputs[:, -1]
@@ -14,7 +19,28 @@ def normal(inputs, model, data, args):
 
 
 def multisource_normal(inputs, model, data, args):
+    '''
+    Likelihood function for data with additive, iid errors sampled from N 
+    normal distributions, each with mean = 0 and std_dev = args[1][n] for
+    n = 1, ..., N. The number of data points sampled from each respective
+    distribution should be provided in args[1]. Both args[0] and args[1] should
+    be aligned and correspond to the ordering of data. That is, if
+    args[0] = (2, 3, 1), then len(data) = 6 and the first two data points
+    are assumed to be drawn from a Normal distribution w/ std_dev = args[1][0].
+    If args[1][n] is None, assumes that the last M columns of inputs contains
+    the std_dev values, where M is the total number of Nones in args[1]. The
+    std_dev columns should be aligned with args[0] in this case.
+
+    :param args: data segment lengths and corresponding standard deviations from
+        Gaussian sample distribution
+    :type args: list of two tuples
+    '''
+    if sum(args[0]) != data.shape[0]:
+        raise ValueError("data segments in args[0] must sum to dim of data")
+
     output = model(inputs)
+
+    args[1], inputs = _process_fixed_and_variable_std(args[1], inputs)
 
     log_likes = []
     start_idx = 0
@@ -32,8 +58,29 @@ def multisource_normal(inputs, model, data, args):
     return np.sum(log_likes, axis=0)
 
 
+def _process_fixed_and_variable_std(std_devs, inputs):
+    '''
+    Identifies standard deviations to be estimated and pulls appropriate
+    samples from the input array.
+    '''
+    num_nones = std_devs.count(None)
+
+    new_std_devs = []
+    for i, std in enumerate(std_devs):
+        if std is None:
+            new_std_devs.append(inputs[:, -num_nones + i])
+        else:
+            new_std_devs.append(std)
+
+    new_inputs = inputs[:, :num_nones]
+
+    return tuple(new_std_devs), new_inputs
+
+
 def _calc_normal_log_like(output, data, var):
     ssqe = np.sum((output - data) ** 2, axis=1)
+
     term1 = -np.log(2 * np.pi * var) * (output.shape[1] / 2.) 
     term2 = -1 / 2. * ssqe / var
+
     return (term1 + term2)
--- tests/unit/test_multisource_likelihood.py
+++ tests/unit/test_multisource_likelihood.py
@@ -4,16 +4,61 @@ import pytest
 from smcpy.log_likelihoods import multisource_normal
 
 
-@pytest.mark.parametrize("args_0", [(1, 7), (8, 0)])
-@pytest.mark.parametrize("args_1", [(1 / np.sqrt(np.pi), 1 / np.sqrt(np.pi))])
+def test_multisource_normal_bad_data_segments():
+    inputs = np.ones((5, 6))
+    data = np.ones(8)
+    model = lambda x: np.ones((x.shape[0], data.shape[0])) * 2
+    args = [(4, 3), (None, None)]
+
+    with pytest.raises(ValueError):
+        multisource_normal(inputs, model, data, args)
+
+
+@pytest.mark.parametrize("args_0,args_1", [((1, 7), (1, 1)),
+                                           ((8, 0), (0.5, 0.5)),
+                                           ((4, 4), (1, 0.5)),
+                                           ((6, 2), (0.5, 1)),
+                                           ((1, 2, 5), (2, 2, 2)),
+                                           ((2, 2, 4), (2, 2, 2)),
+                                           ((2, 2, 4), (1, 2, 3)),
+                                           ((4, 0, 4), (1, 2, 0.5)),
+                                           ((5, 1, 1, 1), (3, 2, 20, 1))])
 def test_multisource_normal_fixed_std(args_0, args_1):
     inputs = np.ones((5, 6))
     data = np.ones(8)
     model = lambda x: np.ones((x.shape[0], data.shape[0])) * 2
     args = [args_0, args_1]
 
-    expected_log_like = -np.log(2) * 8 / 2 - 4 * np.pi
+    segment_array = np.array(args_0)
+    std_array = np.array(args_1)
+    exp_term = sum([np.product(std_array ** 2) / (std_array[i] ** 2)
+                    * segment_array[i] for i in range(len(std_array))])
+    expected_log_like = np.ones(inputs.shape[0])
+    expected_log_like *= (np.log(2 * np.pi) * -(len(data) / 2) + \
+                         np.sum(np.log(std_array) * -segment_array)) + \
+                         -1 / (2 * np.product(std_array ** 2)) * exp_term
 
     log_like = multisource_normal(inputs, model, data, args)
 
-    np.testing.assert_array_equal(log_like, expected_log_like)
+    np.testing.assert_array_almost_equal(log_like, expected_log_like)
+
+
+@pytest.mark.parametrize("args_1", [(None, None, None), (None, 2, 3),
+                                    (1, None, 3), (1, 2, None),
+                                    (None, 2, None), (None, None, 3)])
+def test_multisource_normal_variable_std(args_1):
+    inputs = np.ones((5, 6))
+    data = np.ones(8)
+    model = lambda x: np.ones((x.shape[0], data.shape[0])) * 2
+    args = [(2, 2, 4), args_1]
+
+    none_count = args_1.count(None)
+    for i, std in enumerate(args_1):
+        if std is None:
+            inputs[:, -none_count + i] = i + 1
+
+    expected_log_like = np.ones(inputs.shape[0]) * -14.604474003651934
+
+    log_like = multisource_normal(inputs, model, data, args)
+
+    np.testing.assert_array_almost_equal(log_like, expected_log_like)
