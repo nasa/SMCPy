940ee8857a48be8d2841fd6dfdeaff2c8a115241
Leser <pleser@ndc.nasa.gov>
2021-02-12 11:32:04 -0500

    implemented invwishart prior

--- smcpy/priors.py
+++ smcpy/priors.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from scipy.stats import invwishart
+
 
 class ImproperUniform:
 
@@ -26,3 +28,36 @@ class ImproperUniform:
         prior_pdf = np.zeros(array_x.size)
         in_bounds = (array_x >= self._lower) & (array_x <= self._upper)
         return np.add(prior_pdf, 1, out=prior_pdf, where=in_bounds)
+
+
+
+class InvWishart:
+
+    def __init__(self, cov_dim):
+        self._cov_dim = cov_dim
+        self._dim = cov_dim * (cov_dim + 1) / 2
+        self._invwishart = invwishart(cov_dim, np.eye(cov_dim))
+
+    @property
+    def dim(self):
+        return self._dim
+
+    def rvs(self, num_samples):
+        cov = self._invwishart.rvs(num_samples)
+        idx1, idx2 = np.triu_indices(self._cov_dim)
+        return cov[:, idx1, idx2]
+
+    def pdf(self, x):
+        '''
+        :param x: input array
+        :type x: 2D array (# samples, # unique covariances)
+        '''
+        covs = self._assemble_covs(x)
+        return self._invwishart.pdf(covs)
+
+    def _assemble_covs(self, samples):
+        covs = np.zeros((samples.shape[0], self._cov_dim, self._cov_dim))
+        idx1, idx2 = np.triu_indices(self._cov_dim)
+        covs[:, idx1, idx2] = samples
+        covs += np.transpose(np.triu(covs, 1), axes=(0, 2, 1))
+        return np.transpose(covs, axes=(1, 2, 0))
--- tests/unit/test_priors.py
+++ tests/unit/test_priors.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from smcpy.priors import ImproperUniform
+from smcpy.priors import ImproperUniform, InvWishart
 
 @pytest.mark.parametrize('sign', [-1, 1])
 @pytest.mark.parametrize('x, expected', [(0, np.ones(1)), (1, np.ones(1)),
@@ -31,3 +31,49 @@ def test_improper_uniform_bounds(bounds, expected):
     x = np.array([-1000, -5, -2, 0, 2, 5, 1000])
     prior = ImproperUniform(*bounds)
     np.testing.assert_array_equal(prior.pdf(x), expected)
+
+
+@pytest.mark.parametrize('n', [1, 2, 3, 4])
+def test_invwishart_dim(n):
+    iw = InvWishart(n)
+    assert iw.dim == (n + 1) * n / 2
+
+
+@pytest.mark.parametrize('num_samples', [1, 5, 10])
+def test_invwishart_sample(mocker, num_samples):
+    cov_dim = 3
+    cov_sample = np.array([[0, 1, 2], [1, 3, 4], [2, 4, 5]])
+    expected_sample = np.tile(np.arange(6), (num_samples, 1))
+
+    mock_invwis = mocker.Mock()
+    mock_invwis.rvs.return_value = np.tile(cov_sample, (num_samples, 1, 1))
+    mock_invwis_class = mocker.patch('smcpy.priors.invwishart',
+                                     return_value=mock_invwis)
+
+    iw = InvWishart(cov_dim)
+
+    np.testing.assert_array_equal(iw.rvs(num_samples), expected_sample)
+    mock_invwis.rvs.assert_called_once_with(num_samples)
+    iw_class_call = mock_invwis_class.call_args[0]
+    assert iw_class_call[0] == cov_dim
+    np.testing.assert_array_equal(iw_class_call[1], np.eye(cov_dim))
+
+
+@pytest.mark.parametrize('num_samples', [1, 5, 10])
+def test_invwishart_pdf(mocker, num_samples):
+    cov_dim = 3
+    samples = np.tile(np.arange(6), (num_samples, 1))
+    cov_sample = np.array([[0, 1, 2], [1, 3, 4], [2, 4, 5]])
+    expected_cov = np.tile(cov_sample, (num_samples, 1, 1))
+    expected_cov = np.transpose(expected_cov, axes=(1, 2, 0))
+    expected_prior_probs = np.ones(num_samples)
+
+    mock_invwis = mocker.Mock()
+    mock_invwis.pdf.return_value = np.ones(num_samples)
+    mock_invwis_class = mocker.patch('smcpy.priors.invwishart',
+                                     return_value=mock_invwis)
+
+    iw = InvWishart(cov_dim)
+
+    np.testing.assert_array_equal(iw.pdf(samples), expected_prior_probs)
+    np.testing.assert_array_equal(mock_invwis.pdf.call_args[0][0], expected_cov)
