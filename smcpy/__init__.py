c52233a8751dd64b5613b5cfb45c4373750d89c2
Leser <pleser@ndc.nasa.gov>
2021-04-08 09:24:55 -0400

    updated imports to match develop

--- smcpy/__init__.py
+++ smcpy/__init__.py
@@ -1,4 +1,7 @@
 from .smc_sampler import SMCSampler
-from .priors import ImproperUniform
+from .priors import ImproperUniform, InvWishart
 from .log_likelihoods import Normal, MultiSourceNormal, MVNormal
 from .log_likelihoods import MVNRandomEffects
+from .mcmc.vector_mcmc import VectorMCMC
+from .mcmc.parallel_mcmc import ParallelMCMC
+from .mcmc.vector_mcmc_kernel import VectorMCMCKernel
