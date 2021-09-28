74a704eaeb43857413634995a0b4e69d13bf88c2
Patrick Leser <patrick.e.leser@nasa.gov>
2021-09-28 11:36:41 -0700

    mutator updated for 3d array

--- tests/unit/test_mutator.py
+++ tests/unit/test_mutator.py
@@ -3,22 +3,7 @@ import numpy as np
 import pytest
 
 from smcpy.smc.mutator import Mutator
-from smcpy.smc.particles import Particles
-
-class DummyParticles:
-
-    def __init__(self, params, log_likes, log_weights):
-        self.params = params
-        self.log_likes = log_likes
-        self.log_weights = log_weights
-
-    def compute_covariance(self):
-        return 4
-
-
-@pytest.fixture
-def mutator(stub_mcmc_kernel):
-    return Mutator(mcmc_kernel=stub_mcmc_kernel)
+from smcpy.mcmc.vector_mcmc_kernel import VectorMCMCKernel
 
 
 def test_mcmc_kernel_not_kernel_instance():
@@ -26,29 +11,13 @@ def test_mcmc_kernel_not_kernel_instance():
         initializer = Mutator(None)
 
 
-def test_mutate(mutator, stub_mcmc_kernel, mocker):
-    num_samples = 100
-    cov = 0
-    phi = 1
+def test_mutate(mocker):
+    particles = mocker.Mock()
+    particles_mock = mocker.patch('smcpy.smc.mutator.Particles')
+    mutator = Mutator(mcmc_kernel=mocker.Mock(VectorMCMCKernel))
+    mutator.mcmc_kernel.mutate_particles.return_value = (1, 2, 3)
 
-    mocked_particles = mocker.Mock(Particles, autospec=True)
-    mocked_particles.param_dict = 1
-    mocked_particles.log_likes = 2
-    mocked_particles.log_weights = 3
-    mocker.patch.object(mocked_particles, 'compute_covariance',
-                        return_value=cov)
+    _ = mutator(particles, phi=0.5, num_samples=5)
 
-    mocker.patch('smcpy.smc.mutator.Particles', new=DummyParticles)
-
-    mocker.patch.object(stub_mcmc_kernel, 'mutate_particles',
-                        return_value=[10, 20])
-
-    mutated_particles = mutator.mutate(mocked_particles, phi, num_samples)
-
-    assert mutated_particles.params == 10
-    assert mutated_particles.log_likes == 20
-    assert mutated_particles.log_weights == 3
-
-    stub_mcmc_kernel.mutate_particles.assert_called_with(
-            mocked_particles.param_dict, mocked_particles.log_likes,
-            num_samples, cov, phi)
+    particles.compute_covariance.assert_called_once()
+    particles_mock.called_once_with(1, 2, 3)
