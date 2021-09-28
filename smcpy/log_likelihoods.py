034341639bae7822fd115e090cc4512d6db39258
Patrick Leser <patrick.e.leser@nasa.gov>
2021-09-28 06:20:59 -0700

    updated normal log like calc for 3D input array

--- smcpy/log_likelihoods.py
+++ smcpy/log_likelihoods.py
@@ -5,16 +5,16 @@ import nvtx
 from smcpy.utils import global_imports as gi
 
 
-@cupy.fuse(kernel_name='fused_is_nan_kernel')
+#@cupy.fuse(kernel_name='fused_is_nan_kernel')
 def fused_is_nan(output):
-    return cupy.any(cupy.isnan(output))
+    return gi.num_lib.any(gi.num_lib.isnan(output))
  
 
-@cupy.fuse(kernel_name='calc_normal_log_like_kernel')
+#@cupy.fuse(kernel_name='calc_normal_log_like_kernel')
 def calc_normal_log_like(output, data, var):
-    term1 = cupy.log(2 * cupy.pi * var)
+    term1 = gi.num_lib.log(2 * gi.num_lib.pi * var)
     term2 = (output - data)**2 / var
-    return cupy.sum((term1 + term2) * -0.5, axis=1)
+    return gi.num_lib.sum((term1 + term2) * -0.5, axis=2)
 
 
 class BaseLogLike:
@@ -24,20 +24,18 @@ class BaseLogLike:
         self._data = data
         self._args = args
 
-    @nvtx.annotate(color='green')
+    #@nvtx.annotate(color='green')
     def _get_output(self, inputs):
         output = self._model(inputs)
-
-        with nvtx.annotate(message='isnan'):
-            if gi.USING_GPU:
-                if fused_is_nan(output):
-                    raise ValueError
+        #with nvtx.annotate(message='isnan'):
+        if fused_is_nan(output):
+            raise ValueError
         return output
 
 
 class Normal(BaseLogLike):
 
-    def __init__(self, model, data, args):
+    def __init__(self, model, data, args=None):
         '''
         Likelihood function for data with additive, iid errors sampled from a
         normal distribution with mean = 0 and std_dev = args. If args is None,
@@ -47,165 +45,20 @@ class Normal(BaseLogLike):
 
 
     def __call__(self, inputs):
-        with nvtx.annotate(message='convert inputs'):
-            if gi.USING_GPU:
-                inputs = gi.num_lib.asarray(inputs)
-
-        std_dev = self._args
-        if std_dev is None:
-            std_dev = inputs[:, -1]
-            inputs = inputs[:, :-1]
+        #with nvtx.annotate(message='convert inputs'):
+        inputs = gi.num_lib.asarray(inputs)
+        std_dev = gi.num_lib.expand_dims(inputs[:, :, -1], 2)
+        inputs = inputs[:, :, :-1]
         var = std_dev ** 2
 
-        with nvtx.annotate(message='call get_output'):
-            output = self._get_output(inputs)
-        with nvtx.annotate(message='calc norm log like'):
-            nll = calc_normal_log_like(output, self._data, var.reshape(-1, 1))
-        with nvtx.annotate(message='get'):
-            out_nll = np.empty(inputs.shape[0])
-            out_nll = nll.get(out=out_nll) # makes asynch
-
+        #with nvtx.annotate(message='call get_output'):
+        output = self._get_output(inputs)
+        #with nvtx.annotate(message='calc norm log like'):
+        nll = calc_normal_log_like(output, self._data, var)
+        #with nvtx.annotate(message='get'):
+        out_nll = gi.num_lib.empty((inputs.shape[0], inputs.shape[1], 1))
+        if gi.USING_GPU:
+            nll.get(out=out_nll) # makes asynch
+        else:
+            out_nll[:, :, -1] = nll
         return out_nll
-
-
-class MultiSourceNormal(Normal):
-
-    def __init__(self, model, data, args):
-        '''
-        Likelihood function for data with additive, iid errors sampled from N 
-        normal distributions, each with mean = 0 and std_dev = args[1][n] for
-        n = 1, ..., N. The number of data points sampled from each respective
-        distribution should be provided in args[1]. Both args[0] and args[1]
-        should be aligned and correspond to the ordering of data. That is, if
-        args[0] = (2, 3, 1), then len(data) = 6 and the first two data points
-        are assumed to be drawn from a Normal distribution w/
-        std_dev = args[1][0]. If args[1][n] is None, assumes that the last M
-        columns of inputs contains the std_dev values, where M is the total
-        number of Nones in args[1]. The std_dev columns should be aligned with
-        args[0] in this case.
-
-        :param args: data segment lengths and corresponding standard deviations
-            N from Gaussian distribution
-        :type args: list of two tuples, each of length N
-        '''
-        super().__init__(model, data, args)
-        self._num_nones = self._args[1].count(None)
-
-        if sum(args[0]) != data.shape[0]:
-            raise ValueError("data segments in args[0] must sum to dim of data")
-
-    def __call__(self, inputs):
-
-        std_devs, inputs = self._process_fixed_and_variable_std(inputs)
-
-        output = self._get_output(inputs)
-
-        log_likes = []
-        start_idx = 0
-        for segment_len, segment_std_dev in zip(*[self._args[0], std_devs]):
-
-            if segment_len == 0:
-                continue
-
-            output_segment = output[:, start_idx : start_idx + segment_len]
-            data_segment = self._data[start_idx : start_idx + segment_len]
-            log_likes.append(self._calc_normal_log_like(output_segment,
-                                                        data_segment,
-                                                        segment_std_dev ** 2))
-            start_idx += segment_len
-
-        return np.sum(log_likes, axis=0)
-
-
-    def _process_fixed_and_variable_std(self, inputs):
-        '''
-        Identifies standard deviations to be estimated and pulls appropriate
-        samples from the input array.
-        '''
-        std_devs = self._args[1]
-        new_std_devs = []
-        j = 0
-        for i, std in enumerate(std_devs):
-            if std is None:
-                new_std_devs.append(inputs[:, -self._num_nones + j])
-                j += 1
-            else:
-                new_std_devs.append(std)
-    
-        new_inputs = inputs.copy()
-        if self._num_nones > 0:
-            new_inputs = new_inputs[:, :-self._num_nones]
-    
-        return tuple(new_std_devs), new_inputs
-
-
-class MVNormal(BaseLogLike):
-
-    def __init__(self, model, data, args):
-        '''
-        Likelihood function for data with additive, iid errors sampled from N-D
-        multivariate normal distribution with mean = [0] * N and covariances = 
-        args[n] for n = 1, ..., N where, for example, a 4-D covariance
-        matrix is defined as:
-
-                      [0 1 2 3]
-                cov = [1 4 5 6]  where 0, ..., 7 represent entries in args
-                      [2 3 6 7]
-        
-        If args[n] is None, assumes that the last M columns of inputs
-        contains covariance samples, where M is the total number of Nones in
-        args.
-
-        Data array should have shape (number of snapshots, number of features)
-        where snapshots are independent simultaneous measurements of all the
-        data features. Features, for example, might be sensor readings at
-        different locations.
-
-        :param args: covariances of the N-D multivariate normal
-        :type args: list, len(args) = N * (N + 1) / 2
-        '''
-        super().__init__(model, data, args)
-        self._num_nones = self._args.count(None)
-
-    def __call__(self, inputs):
-        cov_arg_array, inputs = self._process_fixed_and_variable_covar(inputs)
-        cov_matrices = np.tile(self._get_cov(cov_arg_array),
-                               (self._data.shape[0], 1, 1, 1))
-
-        data = np.expand_dims(self._data, 1)
-        output = self._get_output(inputs)
-        error = output - data
-        error = np.expand_dims(error, 2)
-        errorT = np.transpose(error, axes=(0, 1, 3, 2))
-
-        term1 = - self._data.shape[1] / 2 * np.log(2 * np.pi)
-        term2 = - 1 / 2 * np.log(np.linalg.det(cov_matrices))
-        term2 = np.expand_dims(term2, (2, 3))
-        term3 = np.matmul(np.matmul(error, np.linalg.inv(cov_matrices)), errorT)
-
-        log_likes = term1 + term2 + -(1 / 2) * term3
-        log_likes = np.sum(log_likes, axis=0)
-
-        return log_likes[:, :, 0]
-
-    def _get_cov(self, cov_args):
-        d = int((np.sqrt(1 + 8 * cov_args.shape[1]) - 1) / 2)
-        covs = np.zeros((cov_args.shape[0], d, d))
-        p, q = np.triu_indices(d)
-        covs[:, p, q] = cov_args
-        covs += np.transpose(np.triu(covs, 1), axes=[0, 2, 1])
-        return covs
-
-    def _process_fixed_and_variable_covar(self, inputs):
-        covars = np.tile(self._args, (inputs.shape[0], 1))
-        j = 0
-        for i, arg in enumerate(self._args):
-            if arg is None:
-                covars[:, i] = inputs[:, -self._num_nones + j]
-                j += 1
-
-        new_inputs = inputs.copy()
-        if self._num_nones > 0:
-            new_inputs = new_inputs[:, :-self._num_nones]
- 
-        return covars, new_inputs
--- tests/unit/test_likelihoods.py
+++ tests/unit/test_likelihoods.py
@@ -1,132 +1,28 @@
 import numpy as np
 import pytest
 
-from smcpy import MultiSourceNormal, MVNormal, Normal
+from smcpy import Normal
 
 
-@pytest.mark.parametrize('class_, args',
-                         [(MultiSourceNormal, [(3,), (1,)]),
-                          (MVNormal, [0, 0, 0]), (Normal, 1)])
-def test_nan_in_model_raises_value_error(class_, args):
-    model = lambda x: np.array([[1, 2, np.nan], [np.nan, np.nan, 3]])
-    data = np.ones(3)
-    like = class_(model, data, args)
+def test_normal_log_like(mocker):
+    inputs = np.ones((2, 5, 3))
+    data = np.ones(8)
+    model = mocker.Mock(return_value=np.ones((2, 5, 8)) * 2)
 
+    expected_like = np.log(1 / (2 * np.pi) ** 4 * np.exp(-4))
+    expected_like = np.tile(expected_like, (2, 5, 1))
+
+    norm = Normal(model, data)
+    like = norm(inputs)
+
+    np.testing.assert_array_equal(like, expected_like)
+
+
+def test_norm_log_like_nan_output(mocker):
+    inputs = np.ones((2, 5, 3))
+    data = np.ones(8)
+    model = mocker.Mock(return_value=np.array([[[np.nan]], [[1]]]))
+
+    norm = Normal(model, data)
     with pytest.raises(ValueError):
-        like(np.ones((1, 1)))
-
-
-def test_multisource_normal_bad_data_segments():
-    inputs = np.ones((5, 6))
-    data = np.ones(8)
-    model = lambda x: np.ones((x.shape[0], data.shape[0])) * 2
-    args = [(4, 3), (None, None)]
-
-    with pytest.raises(ValueError):
-        MultiSourceNormal(inputs, data, args)
-
-
-@pytest.mark.parametrize("args_0,args_1", [((1, 7), (1, 1)),
-                                           ((8, 0), (0.5, 0.5)),
-                                           ((4, 4), (1, 0.5)),
-                                           ((6, 2), (0.5, 1)),
-                                           ((1, 2, 5), (2, 2, 2)),
-                                           ((2, 2, 4), (2, 2, 2)),
-                                           ((2, 2, 4), (1, 2, 3)),
-                                           ((4, 0, 4), (1, 2, 0.5)),
-                                           ((5, 1, 1, 1), (3, 2, 20, 1))])
-def test_multisource_normal_fixed_std(args_0, args_1):
-    inputs = np.ones((5, 6))
-    data = np.ones(8)
-    model = lambda x: np.ones((x.shape[0], data.shape[0])) * 2
-    args = [args_0, args_1]
-
-    segment_array = np.array(args_0)
-    std_array = np.array(args_1)
-    exp_term = sum([np.product(std_array ** 2) / (std_array[i] ** 2)
-                    * segment_array[i] for i in range(len(std_array))])
-    expected_log_like = np.ones(inputs.shape[0])
-    expected_log_like *= (np.log(2 * np.pi) * -(len(data) / 2) + \
-                         np.sum(np.log(std_array) * -segment_array)) + \
-                         -1 / (2 * np.product(std_array ** 2)) * exp_term
-
-    msn = MultiSourceNormal(model, data, args)
-    log_like = msn(inputs)
-
-    np.testing.assert_array_almost_equal(log_like, expected_log_like)
-
-
-@pytest.mark.parametrize("args_1", [(None, None, None), (None, 2, 3),
-                                    (1, None, 3), (1, 2, None),
-                                    (None, 2, None), (None, None, 3)])
-def test_multisource_normal_variable_std(mocker, args_1):
-    num_samples = 5
-    inputs = [1, 1, 1]
-    data = np.ones(8)
-    model = mocker.Mock(return_value=np.ones((num_samples, 8)) * 2)
-    args = [(2, 2, 4), args_1]
-
-    expected_model_inputs = [1, 1, 1]
-
-    none_count = args_1.count(None)
-    for i, std in enumerate(args_1):
-        if std is None:
-            inputs.append(i + 1)
-
-    inputs = np.tile(inputs, (num_samples, 1))
-
-    expected_log_like = np.ones(inputs.shape[0]) * -14.604474003651934
-    expected_model_inputs = np.ones((5, 3))
-
-    msn = MultiSourceNormal(model, data, args)
-    log_like = msn(inputs)
-
-    np.testing.assert_array_almost_equal(log_like, expected_log_like)
-    np.testing.assert_array_equal(model.call_args_list[0][0][0],
-                                  expected_model_inputs)
-
-
-@pytest.mark.parametrize('n_snapshots', [1, 5, 10])
-@pytest.mark.parametrize('n_samples', [1, 5, 10])
-def test_mvn_likelihood_single_dimension(mocker, n_samples, n_snapshots):
-    model = mocker.Mock(return_value=np.ones((n_samples, 1)))
-    data = np.array([[3]] * n_snapshots)
-    args = [np.sqrt(1 / (2 * np.pi))**2]
-    inputs = np.ones((n_samples, 2))
-
-    expected_log_like = np.array([[-4 * np.pi]] * n_samples) * n_snapshots
-
-    mvn = MVNormal(model, data, args)
-
-    np.testing.assert_array_equal(mvn(inputs), expected_log_like)
-    np.testing.assert_array_equal(model.call_args[0][0], inputs)
-
-
-@pytest.mark.parametrize('n_snapshots', [1, 2, 5])
-@pytest.mark.parametrize('args', [(1, 2, 3, -1, 0, -2),
-                                  (None, 2, 3, -1, 0, None),
-                                  (1, None, None, None, 0, None),
-                                  (None, None, None, None, None, None)])
-@pytest.mark.parametrize('n_samples', [1, 5, 10])
-def test_mvn_likelihood_ndim(mocker, n_samples, n_snapshots, args):
-    n_data_pts = 3
-    n_cov_terms = n_data_pts * (n_data_pts + 1) / 2
-    input_vector = [1, 2]
-    model_output = np.ones((n_samples, n_data_pts))
-
-    base_args = (1, 2, 3, -1, 0, -2)
-    for i, arg in enumerate(args):
-        if arg is None:
-            input_vector.append(base_args[i])
-
-    inputs = np.tile(input_vector, (n_samples, 1))
-
-    model = mocker.Mock(return_value=model_output)
-    data = np.ones((n_snapshots, n_data_pts)) * 2
-    
-    expected_log_like = np.array([[-4.54482456]] * n_samples) * n_snapshots
-
-    mvn = MVNormal(model, data, args)
-
-    np.testing.assert_array_almost_equal(mvn(inputs), expected_log_like)
-    np.testing.assert_array_equal(model.call_args[0][0], inputs[:, :2])
+        _ = norm(inputs)
