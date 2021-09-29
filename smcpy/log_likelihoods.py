fe648f4b73cc8d87843f16f949b48c2082883c00
Patrick Leser <patrick.e.leser@nasa.gov>
2021-09-29 09:06:00 -0700

    added fuse and nvtx back in

--- smcpy/log_likelihoods.py
+++ smcpy/log_likelihoods.py
@@ -5,16 +5,16 @@ import nvtx
 from smcpy.utils import global_imports as gi
 
 
-#@cupy.fuse(kernel_name='fused_is_nan_kernel')
+@cupy.fuse(kernel_name='fused_is_nan_kernel')
 def fused_is_nan(output):
     return gi.num_lib.any(gi.num_lib.isnan(output))
  
 
-#@cupy.fuse(kernel_name='calc_normal_log_like_kernel')
+@cupy.fuse(kernel_name='calc_normal_log_like_kernel')
 def calc_normal_log_like(output, data, var):
     term1 = gi.num_lib.log(2 * gi.num_lib.pi * var)
     term2 = (output - data)**2 / var
-    return gi.num_lib.sum((term1 + term2) * -0.5, axis=2, keepdims=True)
+    return gi.num_lib.sum((term1 + term2) * -0.5, axis=2)
 
 
 class BaseLogLike:
@@ -24,10 +24,8 @@ class BaseLogLike:
         self._data = data
         self._args = args
 
-    #@nvtx.annotate(color='green')
     def _get_output(self, inputs):
         output = self._model(inputs)
-        #with nvtx.annotate(message='isnan'):
         if fused_is_nan(output):
             raise ValueError
         return output
@@ -45,20 +43,22 @@ class Normal(BaseLogLike):
 
 
     def __call__(self, inputs):
-        #with nvtx.annotate(message='convert inputs'):
-        inputs = gi.num_lib.asarray(inputs)
-        std_dev = gi.num_lib.expand_dims(inputs[:, :, -1], 2)
-        inputs = inputs[:, :, :-1]
-        var = std_dev ** 2
+        with nvtx.annotate(message='convert inputs'):
+            inputs = gi.num_lib.asarray(inputs)
+            std_dev = gi.num_lib.expand_dims(inputs[:, :, -1], 2)
+            inputs = inputs[:, :, :-1]
+            var = std_dev ** 2
 
-        #with nvtx.annotate(message='call get_output'):
-        output = self._get_output(inputs)
-        #with nvtx.annotate(message='calc norm log like'):
+        with nvtx.annotate(message='call get_output'):
+            output = self._get_output(inputs)
+
         nll = calc_normal_log_like(output, self._data, var)
-        #with nvtx.annotate(message='get'):
-        out_nll = np.empty((inputs.shape[0], inputs.shape[1], 1))
-        if gi.USING_GPU:
-            nll.get(out=out_nll) # makes asynch
-        else:
-            out_nll[:, :, -1] = nll
+        nll = gi.num_lib.expand_dims(nll, 2)
+
+        with nvtx.annotate(message='get'):
+            out_nll = np.empty((inputs.shape[0], inputs.shape[1], 1))
+            if gi.USING_GPU:
+                nll.get(out=out_nll) # makes asynch
+            else:
+                out_nll[:, :, -1] = nll
         return out_nll
