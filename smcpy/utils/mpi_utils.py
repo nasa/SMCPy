dea112319bc1785047fc198be17634b67961063f
Leser <pleser@ndc.nasa.gov>
2022-03-30 11:52:01 -0400

    updated doc string of mpi util to be correct

--- smcpy/utils/mpi_utils.py
+++ smcpy/utils/mpi_utils.py
@@ -3,14 +3,6 @@ def rank_zero_output_only(func):
     Wrapper function that detects whether mpi4py is available and forces all
     ranks to output from rank 0 only. Intended to be used as a decorator where
     mpi-enabled MCMC kernels are deployed.
-
-    Example use case: in the built-in mpi implementation of MCMC
-    (ParallelMCMC), the likelihoods are synced but particle parameter values
-    are not, meaning that ranks other than rank 0 essentially carry dummy (and
-    fundamentally incorrect) Particles objects. This wrapper ensures that only
-    rank 0 Particles objects are returned to the user. Note that this would not
-    be required if a global random seed is set prior to execution of any SMCPy
-    parallel code.
     """
     def wrapper(self, *args, **kwargs):
         output = func(self, *args, **kwargs)
