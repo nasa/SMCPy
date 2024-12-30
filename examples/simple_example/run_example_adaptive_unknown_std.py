d81678b28c2dbcd698371ab2a517bfe5eb10f288
Patrick Leser <patrick.e.leser@gmail.com>
2024-12-30 15:56:45 -0500

    added correct seeding syntax to simple example, still need to update elsewhere.

--- examples/simple_example/run_example_adaptive_unknown_std.py
+++ examples/simple_example/run_example_adaptive_unknown_std.py
@@ -1,32 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
 from scipy.stats import uniform
 
 from smcpy import AdaptiveSampler, VectorMCMC, VectorMCMCKernel
-from smcpy.utils.plotter import plot_pairwise
 
 from run_example_adaptive import generate_data, eval_model
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    np.random.seed(200)
+    rng = np.random.default_rng(seed=200)
 
-    std_dev = 2
-    noisy_data = generate_data(eval_model, std_dev, plot=False)
+    noisy_data = generate_data(eval_model, std_dev=2, plot=False, rng=rng)
 
-    std_dev = None # estimate it; requires prior for std_dev and param_order
+    std_dev = None  # estimate it; requires prior for std_dev and param_order
 
-    priors = [uniform(0., 6.), uniform(0., 6.), uniform(0, 10)]
+    priors = [uniform(0.0, 6.0), uniform(0.0, 6.0), uniform(0, 10)]
     vector_mcmc = VectorMCMC(eval_model, noisy_data, priors, std_dev)
-    mcmc_kernel = VectorMCMCKernel(vector_mcmc, param_order=('a', 'b', 'std'))
+    mcmc_kernel = VectorMCMCKernel(vector_mcmc, param_order=("a", "b", "std"), rng=rng)
 
     smc = AdaptiveSampler(mcmc_kernel)
-    step_list, mll_list = smc.sample(num_particles=500, num_mcmc_samples=5,
-                                     target_ess=0.8)
+    step_list, mll_list = smc.sample(
+        num_particles=500, num_mcmc_samples=5, target_ess=0.8
+    )
 
-    print('marginal log likelihood = {}'.format(mll_list[-1]))
-    print('parameter means = {}'.format(step_list[-1].compute_mean()))
+    print("marginal log likelihood = {}".format(mll_list[-1]))
+    print("parameter means = {}".format(step_list[-1].compute_mean()))
 
-    plot_pairwise(step_list[-1].params, step_list[-1].weights,
-                  param_names=['a', 'b', 'std'])
+    sns.pairplot(pd.DataFrame(step_list[-1].param_dict))
+    sns.mpl.pyplot.savefig("pairwise.png")
