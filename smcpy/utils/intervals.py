6b22662c911b95674dd558722c0d2e0323a00c64
Julia Truong <juliaTruong75@gmail.com>
2025-02-14 12:03:00 -0500

    added interval sorting and alpha tests

--- smcpy/utils/intervals.py
+++ smcpy/utils/intervals.py
@@ -2,10 +2,13 @@ import numpy as np
 
 
 def compute_intervals(output, alpha):
+    if alpha > 1 or alpha < 0:
+        raise ValueError
     if output.shape == (1, 0):
         return np.array([])
     intervals = []
     for col in output.T:
+        col.sort()
         x = np.linspace(0, 1, len(col))
         intervals.append(np.interp([1 - alpha / 2, alpha / 2], x, col))
     return np.array(intervals).T
