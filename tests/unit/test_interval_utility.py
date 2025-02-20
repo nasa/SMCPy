43d7b9b6e41e571ec82ce7e31991d5676e67ce70
Julia Truong <juliaTruong75@gmail.com>
2025-02-20 09:27:55 -0500

    added testing for noise generation #1

--- tests/unit/test_interval_utility.py
+++ tests/unit/test_interval_utility.py
@@ -37,9 +37,26 @@ def test_credible_interval(alpha, expected_intervals):
         (0.25, np.array([[0.45, 4.5, 45], [0.15, 1.5, 15]])),
     ],
 )
-def test_credible_interval_unordered(alpha, expected_intervals):
+def test_credible_interval_reversed(alpha, expected_intervals):
     output = np.array(
         [[0.5, 5, 50], [0.4, 4, 40], [0.3, 3, 30], [0.2, 2, 20], [0.1, 1, 10]]
+    )
+
+    intervals = compute_intervals(output=output, alpha=alpha)
+
+    np.testing.assert_array_almost_equal(intervals, expected_intervals)
+
+
+@pytest.mark.parametrize(
+    "alpha, expected_intervals",
+    [
+        (0.5, np.array([[0.4, 4, 40], [0.2, 2, 20]])),
+        (0.25, np.array([[0.45, 4.5, 45], [0.15, 1.5, 15]])),
+    ],
+)
+def test_credible_interval_unordered(alpha, expected_intervals):
+    output = np.array(
+        [[0.4, 4, 40], [0.1, 1, 10], [0.5, 5, 50], [0.2, 2, 20], [0.3, 3, 30]]
     )
 
     intervals = compute_intervals(output=output, alpha=alpha)
