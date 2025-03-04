645b18edb081c4f7a1da37262bf00379bdbcea9e
Julia Truong <juliaTruong75@gmail.com>
2025-03-04 14:11:55 -0500

    cleaned up compute intervals func

--- smcpy/utils/intervals.py
+++ smcpy/utils/intervals.py
@@ -4,8 +4,7 @@ import numpy as np
 def compute_intervals(output, alpha):
     if alpha > 1 or alpha < 0:
         raise ValueError
-    if output.shape == (1, 0):
-        return np.array([])
+
     intervals = []
     for col in output.T:
         col.sort()
--- tests/unit/test_interval_utility.py
+++ tests/unit/test_interval_utility.py
@@ -65,10 +65,26 @@ def test_credible_interval_unordered(alpha, expected_intervals):
 
 
 @pytest.mark.parametrize("alpha_invalid", [-1, -0.5, 2, -2])
-def test_credible_alpha(alpha_invalid):
+def test_valid_alpha(alpha_invalid):
     output = np.array(
         [[0.5, 5, 50], [0.4, 4, 40], [0.3, 3, 30], [0.2, 2, 20], [0.1, 1, 10]]
     )
 
     with pytest.raises(ValueError):
         compute_intervals(output=output, alpha=alpha_invalid)
+
+
+@pytest.mark.parametrize(
+    "alpha, expected_intervals",
+    [
+        (0, np.array([[0.5, 5, 50], [0.1, 1, 10]])),
+        (1, np.array([[0.3, 3, 30], [0.3, 3, 30]])),
+    ],
+)
+def test_edge_cases_alpha(alpha, expected_intervals):
+    output = np.array(
+        [[0.5, 5, 50], [0.4, 4, 40], [0.3, 3, 30], [0.2, 2, 20], [0.1, 1, 10]]
+    )
+
+    intervals = compute_intervals(output=output, alpha=alpha)
+    np.testing.assert_array_almost_equal(intervals, expected_intervals)
