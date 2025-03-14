374810eafd1e5a9f4955b4dca68635207fa8c4b8
Julia Truong <juliaTruong75@gmail.com>
2025-03-14 08:54:29 -0400

    applied mpi tests feedback #19

--- tests/unit/test_mpi_utils.py
+++ tests/unit/test_mpi_utils.py
@@ -1,4 +1,5 @@
 import pytest
+
 from smcpy.utils.mpi_utils import *
 
 
@@ -18,7 +19,6 @@ def test_rank_zero_output_mpi_comm(mocker):
     res = mpi_output_mock.mpi_func()
 
     assert res == 0
-    # check arguments of bcast
     comm.bcast.assert_called_once_with(1, root=0)
 
 
@@ -26,7 +26,7 @@ def test_rank_zero_output_kernel_comm(mocker):
     mcmc_kernel = mocker.Mock()
     mcmc_kernel._mcmc._comm.bcast.return_value = 0
 
-    class NoMPIOutputMock:
+    class MPIOutputMock:
         def __init__(self, mcmc_kernel):
             self._mcmc_kernel = mcmc_kernel
 
@@ -34,7 +34,7 @@ def test_rank_zero_output_kernel_comm(mocker):
         def no_mpi_func(self):
             return 1
 
-    no_mpi_output_mock = NoMPIOutputMock(mcmc_kernel)
+    no_mpi_output_mock = MPIOutputMock(mcmc_kernel)
     res = no_mpi_output_mock.no_mpi_func()
 
     assert res == 0
@@ -42,19 +42,15 @@ def test_rank_zero_output_kernel_comm(mocker):
 
 
 def test_rank_zero_output_no_comms():
-    class NoCommsMock:
+    class NoCommMock:
         @rank_zero_output_only
         def no_comms_func(self):
             return 1
 
-    no_comms_mock = NoCommsMock()
+    no_comms_mock = NoCommMock()
     res = no_comms_mock.no_comms_func()
 
     assert res == 1
-    with pytest.raises(AttributeError):
-        no_comms_mock._comms
-    with pytest.raises(AttributeError):
-        no_comms_mock._mcmc_kernel
 
 
 class MPIRunMock:
