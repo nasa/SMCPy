fixed MVNHierarchicalModel covariance construction #30

smcpy/hierarch_log_likelihoods.py
@@ -72,9 +72,8 @@ class MVNHierarchModel:
         dim = self._inputs.shape[1]
         cov = np.zeros((self._inputs.shape[0], dim, dim))
         i, j = np.triu_indices(dim)
-        k, l = np.tril_indices(dim)
         cov[:, i, j] = self._hyperparams
-        cov[:, k, l] = self._hyperparams
+        cov[:, j, i] = self._hyperparams
         return cov
 
     def _compute_squared_mahalanobis(self, data):

tests/unit/test_hierarchical_likelihoods.py
@@ -58,8 +58,28 @@ def test_approx_hierarch_call(mocker):
     np.testing.assert_array_almost_equal(log_likes, expected_log_likes)
 
 
-@pytest.mark.parametrize("n_inputs,n_hyper", [(2, 3), (4, 10)])
-def test_hierarch_mvnormal_model_init(n_inputs, n_hyper):
+@pytest.mark.parametrize(
+    "n_inputs, n_hyper, expected_cov",
+    [
+        (2, 3, np.array([[[2.0, 3.0], [3.0, 4.0]]] * 10)),
+        (
+            4,
+            10,
+            np.array(
+                [
+                    [
+                        [4.0, 5.0, 6.0, 7.0],
+                        [5.0, 8.0, 9.0, 10.0],
+                        [6.0, 9.0, 11.0, 12.0],
+                        [7.0, 10.0, 12.0, 13.0],
+                    ]
+                ]
+                * 10
+            ),
+        ),
+    ],
+)
+def test_hierarch_mvnormal_model_init(n_inputs, n_hyper, expected_cov):
     n_samples = 10
     n_total = n_inputs + n_hyper
     inputs = np.tile(np.arange(n_total), (n_samples, 1))
@@ -67,6 +87,7 @@ def test_hierarch_mvnormal_model_init(n_inputs, n_hyper):
 
     np.testing.assert_array_equal(model._inputs, inputs[:, :n_inputs])
     np.testing.assert_array_equal(model._hyperparams, inputs[:, n_inputs:])
+    np.testing.assert_array_equal(model._cov, expected_cov)
 
 
 @pytest.mark.parametrize("n_inputs,n_hyper", [(1, 3), (7, 5), (15, 294)])