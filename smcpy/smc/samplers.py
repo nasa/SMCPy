fixed estimated time calculation for interp

smcpy/smc/samplers.py
@@ -354,8 +354,10 @@ class FixedTimeSampler(AdaptiveSampler):
 
     def optimize_step(self, particles, phi_old, target_ess=1):
         if len(self._time_history) >= 2:
-            estimated_future_time = 2 * (self.prev_time_step) + self._time_history[-1]
-            if estimated_future_time >= self.final_time:
+            estimated_two_step_future_time = (
+                2 * (self.prev_time_step) + self._time_history[-1]
+            )
+            if estimated_two_step_future_time >= self.final_time:
                 return 1
 
         curr_adaptive_phi = super().optimize_step(
@@ -363,8 +365,7 @@ class FixedTimeSampler(AdaptiveSampler):
         )
 
         if self._buffer_phi:
-            estimated_future_time = 2 * self.prev_time_step
-
+            estimated_future_time = self.prev_time_step + self._time_history[-1]
             return 10 ** max(
                 np.log10(curr_adaptive_phi),
                 np.interp(