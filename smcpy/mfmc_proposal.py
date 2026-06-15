83f87ddf171ca4fda64517f0ab6b5801a20a3cbd
Victor Alejandro Sanchez <vsanchez@andrew.cmu.edu>
2026-06-15 10:45:47 -0400

    more informative variable names #1

--- smcpy/mfmc_proposal.py
+++ smcpy/mfmc_proposal.py
@@ -11,25 +11,25 @@ class MultiFidelityProposal:
     """
 
     def __init__(self, 
-                 lofi_samples: np.ndarray, 
+                 lofi_particles: np.ndarray, 
                  lofi_model: Callable, 
                  observed_data: np.ndarray, 
-                 noise_stdev: float, 
-                 priors: List[Any]):
+                 priors: List[Any],
+                 additive_noise_stdev: Optional[float] = None):
         """
         Initializes the Multi-Fidelity proposal distribution.
 
         Args:
-            lofi_samples (np.ndarray): Accepted posterior samples from the LF SMC run.
+            lofi_particles (np.ndarray): Accepted posterior samples from the LF SMC run.
             lofi_model (Callable): The LF model used to evaluate simulated outputs.
-            data (np.ndarray): The true observed measurement data being targeted.
-            noise_stdev (float): Standard deviation of the Gaussian noise.
-            prior (List[Any]): List of prior distribution objects for the parameters.
+            observed_data (np.ndarray): The true observed measurement data being targeted.
+            priors (List[Any]): List of prior distribution objects for the parameters.
+            additive_noise_stdev (Optional[float]): Standard deviation of the Gaussian noise.
         """
-        self.lofi_samples = lofi_samples
+        self.lofi_particles = lofi_particles
         self.lofi_model = lofi_model
         self.observed_data = observed_data
-        self.noise_stdev = noise_stdev
+        self.additive_noise_stdev = additive_noise_stdev
         self._priors_list = priors
         self._dims = self._get_dims()
 
@@ -44,49 +44,49 @@ class MultiFidelityProposal:
         Returns:
             np.ndarray: Subsampled particles of shape (num_samples, N_parameters).
         """
-        if num_samples == len(self.lofi_samples):
-            return self.lofi_samples
+        if num_samples == len(self.lofi_particles):
+            return self.lofi_particles
             
         if random_state is not None:
             np.random.seed(random_state)
 
-        random_indices = np.random.choice(self.lofi_samples.shape[0], size=num_samples, replace=False)
-        return self.lofi_samples[random_indices]
+        random_indices = np.random.choice(self.lofi_particles.shape[0], size=num_samples, replace=False)
+        return self.lofi_particles[random_indices]
 
-    def logpdf(self, inputs: np.ndarray) -> np.ndarray:
+    def logpdf(self, particles: np.ndarray) -> np.ndarray:
         """
         Evaluates the unnormalized log probability density of the proposed particles.
 
         Args:
-            inputs (np.ndarray): Proposed parameter sets of shape (num_samples, N_parameters).
+            particles (np.ndarray): Proposed parameter sets of shape (num_samples, N_parameters).
 
         Returns:
             np.ndarray: Unnormalized log LF posterior density of shape (num_samples, 1).
         """
         # 1. Evaluate the log-prior
-        log_prior_values = self.prior_logpdf(inputs)
+        log_prior_values = self.prior_logpdf(particles)
         log_prior_values = np.array(log_prior_values).reshape(-1, 1)
         
         # 2. Evaluate the LF log-likelihood
-        log_likelihood = Normal(self.lofi_model, self.observed_data, self.noise_stdev)
-        log_L_values = np.array(log_likelihood(inputs)).reshape(-1, 1)
+        log_likelihood = Normal(self.lofi_model, self.observed_data, self.additive_noise_stdev)
+        log_likelihood_values = np.array(log_likelihood(particles)).reshape(-1, 1)
 
         # 3. Sum prior and likelihood to get the unnormalized log LF posterior density
-        return log_prior_values + log_L_values
+        return log_prior_values + log_likelihood_values
     
-    def prior_logpdf(self, inputs: np.ndarray) -> np.ndarray:
+    def prior_logpdf(self, particles: np.ndarray) -> np.ndarray:
         """
-        Computes the joint log-prior probability for the inputs.
+        Computes the joint log-prior probability for the particles.
 
         Args:
-            inputs (np.ndarray): Parameter sets to evaluate.
+            particles (np.ndarray): Parameter sets to evaluate.
 
         Returns:
             np.ndarray: Joint log-prior probabilities of shape (num_samples, 1).
         """
-        iterable = zip(self._priors_list, self._partition_inputs(inputs))
-        indv_logpdf = np.hstack([d.logpdf(in_) for d, in_ in iterable])
-        return np.sum(indv_logpdf, axis=1, keepdims=True)
+        iterable = zip(self._priors_list, self._partition_inputs(particles))
+        marginal_logpdfs = np.hstack([prior.logpdf(partial_particle) for prior, partial_particle in iterable])
+        return np.sum(marginal_logpdfs, axis=1, keepdims=True)
     
     def _get_dims(self) -> List[int]:
         """
@@ -95,16 +95,16 @@ class MultiFidelityProposal:
         Returns:
             List[int]: A list of parameter dimension sizes.
         """
-        return [d.rvs(1).size for d in self._priors_list]
+        return [prior.rvs(1).size for prior in self._priors_list]
 
-    def _partition_inputs(self, inputs: np.ndarray) -> List[np.ndarray]:
+    def _partition_inputs(self, particles: np.ndarray) -> List[np.ndarray]:
         """
         Splits the input array along columns to match individual prior dimensions.
 
         Args:
-            inputs (np.ndarray): Full parameter array.
+            particles (np.ndarray): Full parameter array.
 
         Returns:
             List[np.ndarray]: A list of sub-arrays corresponding to each prior.
         """
-        return np.split(inputs, np.cumsum(self._dims)[:-1], axis=1)
+        return np.split(particles, np.cumsum(self._dims)[:-1], axis=1)
